a5b097590c8247dacf857888172a31f8b7402d1d
Ji Huang <cocafehj@gmail.com>
2019-09-14 17:11:10 +0800

    exynos-hotplug: fix cross referencing, potentially dead loop bug
    When register actually the _same_ notifier block to different notifier
    chains, that actually make these notifier chains join together which is
    absolutely unwanted.
    
            chain: min +---> ... +---> foo +---> null
                                        ^
                                        |
            chain: max +---> ... +------+
    
    Then if one more this register pattern exists in some other code, it will
    cause dead loop when walking through related notifier chains.
    
        chain_add(min, new);
    
            chain: min +---> ... +---> foo +---> new +---> null
                                        ^
                                        |
            chain: max +---> ... +------+
    
        chain_add(max, new);
        notifier_chain_register() will result following:
    
            chain: min +---> ... +---> foo +---> new +---+
                                        ^         ^      |
                                        |         |      |
            chain: max +---> ... +------+         +------+
    
    Signed-off-by: Ji Huang <cocafehj@gmail.com>

--- drivers/soc/samsung/exynos-cpu_hotplug.c
+++ drivers/soc/samsung/exynos-cpu_hotplug.c
@@ -301,7 +301,11 @@ static int cpu_hotplug_qos_handler(struct notifier_block *b,
 	return do_cpu_hotplug(false);
 }
 
-static struct notifier_block cpu_hotplug_qos_notifier = {
+static struct notifier_block cpu_hotplug_min_qos_notifier = {
+	.notifier_call = cpu_hotplug_qos_handler,
+};
+
+static struct notifier_block cpu_hotplug_max_qos_notifier = {
 	.notifier_call = cpu_hotplug_qos_handler,
 };
 
@@ -432,8 +436,8 @@ static struct pm_qos_request boot_min_cpu_hotplug_request;
 static void __init cpu_hotplug_pm_qos_init(void)
 {
 	/* Register PM QoS notifier handler */
-	pm_qos_add_notifier(PM_QOS_CPU_ONLINE_MIN, &cpu_hotplug_qos_notifier);
-	pm_qos_add_notifier(PM_QOS_CPU_ONLINE_MAX, &cpu_hotplug_qos_notifier);
+	pm_qos_add_notifier(PM_QOS_CPU_ONLINE_MIN, &cpu_hotplug_min_qos_notifier);
+	pm_qos_add_notifier(PM_QOS_CPU_ONLINE_MAX, &cpu_hotplug_max_qos_notifier);
 
 	/* Guarantee all CPUs running during booting time */
 	pm_qos_add_request(&boot_min_cpu_hotplug_request,
