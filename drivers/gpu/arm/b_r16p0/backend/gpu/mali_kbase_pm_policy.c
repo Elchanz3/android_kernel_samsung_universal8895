ce24f3b0b4aed6a132ab426ef9ccf9b45bc048b4
snapdragon teaks <inutilidades639@gmail.com>
2025-06-21 22:09:41 -0300

    gpu : r16p0 : use aways_on as default power policy
    coarse_demand causes a delay when turning the cores on/off, so as a way of mitigating this delay we'll use always_on as the default, at the cost of minimal energy consumption.

--- drivers/gpu/arm/b_r16p0/backend/gpu/mali_kbase_pm_policy.c
+++ drivers/gpu/arm/b_r16p0/backend/gpu/mali_kbase_pm_policy.c
@@ -32,18 +32,8 @@
 
 static const struct kbase_pm_policy *const all_policy_list[] = {
 #ifdef CONFIG_MALI_NO_MALI
+	&kbase_pm_coarse_demand_policy_ops,
 	&kbase_pm_always_on_policy_ops,
-	&kbase_pm_coarse_demand_policy_ops,
-#if !MALI_CUSTOMER_RELEASE
-	&kbase_pm_always_on_demand_policy_ops,
-#endif
-#else				/* CONFIG_MALI_NO_MALI */
-	&kbase_pm_coarse_demand_policy_ops,
-#if !MALI_CUSTOMER_RELEASE
-	&kbase_pm_always_on_demand_policy_ops,
-#endif
-	&kbase_pm_always_on_policy_ops
-#endif /* CONFIG_MALI_NO_MALI */
 };
 
 /* A filtered list of policies available in the system, calculated by filtering
